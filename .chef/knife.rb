633659b7ffd65eaabb1d58719facd6989d1c09c9
Steven Carlton <scarlton@navisite.com>
2014-04-11 15:26:59 -0400

    knife config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,13 @@
-cookbook_path [ '/root/chef-repo/cookbooks' ]
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "scarlton"
+client_key               "#{current_dir}/scarlton.pem"
+validation_client_name   "rockstar_digital-validator"
+validation_key           "#{current_dir}/rockstar_digital-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/rockstar_digital"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
